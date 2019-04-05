5a30715bd90ba70d9a95e6caf4f0763ecd28dd4a
Lillian Dixon <dixon.lillian92@gmail.com>
2019-04-05 11:18:27 -0600

    'testing'

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -6,6 +6,7 @@ export default function(){
         <div className="home">
             <img src={JHLogo}/>
             <h1>Joanne Hall Art</h1>
+            {console.log(process.env.CLOUDINARY_UPLOAD_URL)}
         </div>
              
     )
