baf2869aa6788b8b4932ceb261783e65d91a0c08
Lillian Dixon <dixon.lillian92@gmail.com>
2019-04-05 11:07:35 -0600

    testing

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -26,14 +26,16 @@ export default class Login extends Component {
 
     handleSubmit(event) {
       event.preventDefault();
+      console.log(email)
+      console.log(password)
 
-      if(this.state.email === EMAIL && this.state.password === PASSWORD){
-          console.log('you can come in')
-          this.props.handleSuccessfulAuth();
-        }else{
-          console.log('error')
-          // this.props.handleUnSuccessfulAuth();
-        }
+      // if(this.state.email === EMAIL && this.state.password === PASSWORD){
+      //     console.log('you can come in')
+      //     this.props.handleSuccessfulAuth();
+      //   }else{
+      //     console.log('error')
+      //     // this.props.handleUnSuccessfulAuth();
+      //   }
     }
 
   render() {
